18c9d5a096138600d0d66ee34899b62533339883
RickyC0626 <rickychon99@gmail.com>
2020-05-31 22:42:00 -0400

    Add function template for countBs

---
+++ 03-countBs/countBs.js
@@ -0,0 +1,10 @@
+/*
+    Write a function `countBs` that takes a string as its only argument and 
+    returns a number that indicates how many uppercase "B" characters there are in the string.
+*/
+function countBs(word)
+{
+
+}
+
+module.exports = countBs;
