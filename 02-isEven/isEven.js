bf2446b19283207739299279f43d67f0381c7473
RickyC0626 <rickychon99@gmail.com>
2020-05-31 22:41:36 -0400

    Add function template for isEven

---
+++ 02-isEven/isEven.js
@@ -0,0 +1,10 @@
+/*
+    Define a recursive function `isEven` that takes in a single parameter (a positive, whole number) and 
+    returns a Boolean. True if even, false otherwise.
+*/
+function isEven(num)
+{
+
+}
+
+module.exports = isEven;
