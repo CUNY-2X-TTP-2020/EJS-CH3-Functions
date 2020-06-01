c2b45d0a2ce048addcc412ea6aaa8292f79ee5a8
RickyC0626 <rickychon99@gmail.com>
2020-05-31 22:42:14 -0400

    Add function template for countChar

---
+++ 04-countChar/countChar.js
@@ -0,0 +1,10 @@
+/*
+    Write a function called `countChar` that behaves like `countBs`, except it 
+    takes a second argument that indicates the character that is to be counted.
+*/
+function countChar(word, char)
+{
+
+}
+
+module.exports = countChar;
